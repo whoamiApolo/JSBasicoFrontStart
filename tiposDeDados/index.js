f719d2c0e6924c10967b5e40352fb5af25275c08
Apolo <whoami.apolo@gmail.com>
2021-10-30 16:34:37 -0300

    tipos de dados JS

---
+++ tiposDeDados/index.js
@@ -0,0 +1,34 @@
+let h1, h2, p, ex, ex1, ex2
+h1 = "- Anotações Simples"
+h2 = "- Tipagem dinâmica"
+p = "JavaScript é uma linguagem de tipagem dinâmica. Isso significa que você não necessita declarar o tipo de uma variável antes de sua atribuição. O tipo será automaticamente determinado quando o programa for processado. Isso também significa que você pode reatribuir uma mesma variável com um tipo diferente:"
+ex = "var foo = 42"
+ex1 = 'foo = "bar"'
+ex2 = 'foo = "true"'
+
+console.log(h1)
+console.log(h2)
+console.log(p)
+console.log(ex + "\n" + ex1 + "\n" + ex2 )
+ 
+h2 = "- Tipos de Dados:"
+console.log(h2)
+p = "São sete tipos de dados primitivos:"
+console.log(p)
+p = "° Boolean"
+console.log(p)
+p = "° Null"
+console.log(p)
+p = "° Undefined"
+console.log(p)
+p = "° Number"
+console.log(p)
+p = "° BigInt"
+console.log(p)
+p = "° String"
+console.log(p)
+p = "° Symbol"
+console.log(p)
+p = "e Object"
+console.log(p)
+
