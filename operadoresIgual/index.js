21f24ccdd70c376e2f363a8bfeeb450426bb8c71
Apolo <whoami.apolo@gmail.com>
2022-01-11 15:40:03 -0300

    igual & não igual

---
+++ operadoresIgual/index.js
@@ -0,0 +1,8 @@
+const number = "1"
+const ola = "HELLO WORLD"
+
+console.log(ola === "hello world") // false
+console.log(ola.toLowerCase() === "hello world") // true
+
+console.log(number === 1) // false
+console.log(number !== 1) // true
