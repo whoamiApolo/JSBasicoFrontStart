770bf189381eb481f824e92a9df57b18037e2a51
Apolo <whoami.apolo@gmail.com>
2022-01-11 01:57:00 -0300

    if & else if & else

---
+++ elseIf/index.js
@@ -0,0 +1,11 @@
+const frutas = "uva"
+
+if (frutas === "uva") {
+    console.log("uva")
+} else if(frutas === "maça") {
+    console.log("maça")
+} else if(frutas === "pera"){
+    console.log("pera")
+} else {
+    console.log("nulo")
+}
