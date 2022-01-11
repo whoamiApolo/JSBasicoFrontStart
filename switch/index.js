1af5c69c305cf689e8fca2b5af49fd6ce7ad3cef
Apolo <whoami.apolo@gmail.com>
2022-01-11 01:47:00 -0300

    switchCase

---
+++ switch/index.js
@@ -0,0 +1,16 @@
+const activeTab = 1
+
+switch (activeTab) {
+    case 0:
+        console.log("activeTab equivale a zero")
+        break;
+    case 1:
+        console.log("number 1")
+        break;
+    case 2:
+        console.log("number 2")
+        break;
+    default:
+        console.log("default")
+        break;
+}
