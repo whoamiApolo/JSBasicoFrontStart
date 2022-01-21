5011a60704c751e8141f9a207bb8160be1639f52
Apolo <whoami.apolo@gmail.com>
2022-01-21 20:05:08 -0300

    slider indicador de posição

--- Slide/index.js
+++ Slide/index.js
@@ -3,6 +3,7 @@ const nextElement = document.getElementById('next')
 const sliderElement = document.getElementById('slider')
 let interval = undefined;
 let timeout = undefined
+let selectedImgIndex = 0
 
 previousElement.addEventListener('click', onPreviousClick)
 nextElement.addEventListener('click', onNextClick)
@@ -12,12 +13,16 @@ autoScroll()
 function onPreviousClick() {
     const sliderWidth = sliderElement.offsetWidth
     sliderElement.scrollLeft -= sliderWidth
+    --selectedImgIndex
+    handleActiveDot()
     handleSliderClick()
 }
 
 function onNextClick() {
     const sliderWidth = sliderElement.offsetWidth
     sliderElement.scrollLeft += sliderWidth
+    ++selectedImgIndex
+    handleActiveDot()
     handleSliderClick()
 }
 
@@ -30,21 +35,33 @@ function handleSliderClick() {
     }, 20000);
 }
 
+function handleActiveDot() {
+    const list = Array.from(document.getElementsByClassName('dot'))
+    
+    if(selectedImgIndex < 0 ) selectedImgIndex = 0
+    if(selectedImgIndex >= list.length) selectedImgIndex = list.length - 1
+
+    list.forEach(element => element.classList.remove('active'));
+    list[selectedImgIndex].classList.add('active')
+}
+
 function autoScroll() {
-    if(interval) return
-    let i = 0
-        interval = setInterval(() => {
-            console.log('-->', i)
-            ++i
+    if (interval) return
+    interval = setInterval(() => {
+
         const sliderWidth = sliderElement.offsetWidth
         const numberOfImages = sliderElement.childElementCount
-        const selectedImage = (sliderElement.scrollLeft/sliderWidth) + 1
-        
-        if(numberOfImages === selectedImage) {
+        const selectedImage = (sliderElement.scrollLeft / sliderWidth) + 1
+
+        if (numberOfImages === selectedImage) {
             sliderElement.scrollLeft = 0
+            selectedImgIndex = 0
+            handleActiveDot()
             return
         }
 
         sliderElement.scrollLeft += sliderWidth
+        ++selectedImgIndex
+        handleActiveDot()
     }, 5000);
 }
